0575967da3e136712e56d14e0a3ea4c98dcad3e0
nizam <nizampnizam7@gmail.com>
2025-06-24 11:02:35 +0530

    added filtering controller to backend and finished backend

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -21,35 +21,44 @@ exports.createTransaction = async (req, res) => {
     }
 }
 
-exports.getTransaction = async(req,res)=>{
+exports.getTransaction = async (req, res) => {
     try {
-        const transactions = await Transaction.find({userId:req.userId});
-        res.status(200).json({ transactions});
+        const { category, start, end } = req.query;
+        let filter = { userId: req.userId };
+        if (category) filter.category = category;
+        if (start || end) {
+            filter.date = {}
+            if (start) filter.date.$gte = new Date(start);
+            if (end) filter.date.$lte = new Date(end);
+        }
+        const transactions = await Transaction.find(filter);
+        res.status(200).json({ transactions });
+        
     } catch (error) {
-          res.status(500).json({ message: 'retrieving transactions failed',error });  
+        res.status(500).json({ message: 'retrieving transactions failed', error });
     }
 }
 
-exports.editTransaction = async (req,res)=>{
+exports.editTransaction = async (req, res) => {
     try {
         const transaction = await Transaction.findOneAndUpdate(
-            {_id:req.params.id,userId:req.userId},
+            { _id: req.params.id, userId: req.userId },
             req.body,
-            {new:true}
+            { new: true }
         );
         res.status(201).json({ message: "transaction updated", data: transaction })
     } catch (error) {
-        
+
     }
 }
 
-exports.deleteTransaction = async (req,res)=>{
+exports.deleteTransaction = async (req, res) => {
     try {
         await Transaction.findOneAndDelete(
-            {_id:req.params.id,userId: req.userId}
+            { _id: req.params.id, userId: req.userId }
         );
-        res.status(200).json({message:"transaction deleted"})
+        res.status(200).json({ message: "transaction deleted" })
     } catch (error) {
-        res.status(500).json({ msg: 'failed to delete transaction',error });
+        res.status(500).json({ msg: 'failed to delete transaction', error });
     }
 }
